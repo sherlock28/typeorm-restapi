9530ec60d77ec689c7e9232d300f9d5efb2ebc5f
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-06-26 06:47:07 -0300

    Added: Database config connection

---
+++ src/config/db.ts
@@ -0,0 +1,23 @@
+import { DataSource } from 'typeorm';
+
+const defaultConfig = {
+    pghost: "localhost",
+    pgport: 5432,
+    pgdatabase: "testdb",
+    pguser: "postgres",
+    pgpassword: "postgres"
+}
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: process.env.PGHOST || defaultConfig.pghost,
+    port:  process.env.PGPORT === undefined ? defaultConfig.pgport : +process.env.PGPORT,
+    username: process.env.PGUSER || defaultConfig.pguser,
+    password: process.env.PGPASSWORD || defaultConfig.pgpassword,
+    database: process.env.PGDATABASE || defaultConfig.pgdatabase,
+    synchronize: true,
+    logging: process.env.NODE_ENV === "development" ? true : false,
+    entities: [],
+    subscribers: [],
+    migrations: [],
+})
