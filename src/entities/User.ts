3b972d7fbfe9950275a6969e28e3cfaca39887f8
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-06-26 07:28:24 -0300

    Added: User Model

---
+++ src/entities/User.ts
@@ -0,0 +1,25 @@
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity, CreateDateColumn, UpdateDateColumn } from "typeorm"
+
+@Entity()
+export class User extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    first_name: string
+
+    @Column()
+    last_name: string
+
+    @Column()
+    age: number
+
+    @Column()
+    is_active: boolean
+
+    @CreateDateColumn()
+    created_at: Date
+
+    @UpdateDateColumn()
+    updated_at: Date
+}
